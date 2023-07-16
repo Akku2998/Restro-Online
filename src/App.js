89d23e6fc1bcd48337e0750709a9f70c63f2d10a
nidhi <aakritinidhi123@gmail.com>
2023-07-16 16:55:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Frontpage from './Assesment_Techverse/FrontPage'
+//import UserDetails from './UserProfile/UserDetails';
+//import { Route,Routes } from  'react-router-dom';
+/*import Home from './PublicRoutes/Home';
+import About from './PublicRoutes/About';
+import Incdec from './ReduxApp/Incdec';
+import Errorr from './PublicRoutes/Errorr';
+import Navbar from './PublicRoutes/Navbar';
+import User from './UserInfo/User';
+import UserId from './UserInfo/UserId';
+import Author from './BlogPost/Author';
+import AuthorDetails from './BlogPost/AuthorDetails';*/
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Frontpage/>
+  
+      
+      {/*<Exercise1/>*/}
+    
+          {/*<Exercise1 imgsrc={AllData[0].imgsrc} Name={AllData[0].Name} Id={AllData[0].Id} />
+          <Exercise1 imgsrc={AllData[1].imgsrc} Name={AllData[1].Name} Id={AllData[1].Id}  />
+          <Exercise1 imgsrc={AllData[2].imgsrc} Name={AllData[2].Name} Id={AllData[2].Id}  />
+          <Exercise1 imgsrc={AllData[3].imgsrc} Name={AllData[3].Name} Id={AllData[3].Id}  />*/}
     </div>
-  );
+  
+  )
 }
 
 export default App;
