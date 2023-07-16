69e81f7c6f10983dbafabea33d2119396802b661
Nidhi Gupta <aakritinidhi123@gmail.com>
2023-07-16 22:17:43 +0530

    code refactor

--- src/components/ExploreFood.jsx
+++ src/components/ExploreFood.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import React from "react";
 
-function ExploreFood (props)  {
+function ExploreFood(props) {
   return (
     <>
-    <div className="w-full h-auto">
-    <div className="card m-6 transition-all duration-400 ease-in bg-white w-1/5 rounded-2xl shadow-md float-left">
-    <img src={props.imgsrc} alt="mypic" className="card_img w-full h-235 border-tl-12 border-tr-12" />
-    <div className="card_info bg-white border-tl-12 border-tr-12 p-6">
-    <h3 className="card_title mt-5 mb-10 font-raleway bg-white capitalize">{props.fname}  <span className="ml-16">{props.price}</span></h3>
-    
-    <span className="card_category uppercase font-raleway text-xs tracking-widest font-medium text-gray-600 bg-white">{props.title}</span>
-   
+      <div className="h-auto w-1/4 mx-4">
+        <div className="m-6 bg-white rounded-2xl shadow-md">
+          <img
+            src={props.imgsrc}
+            alt="mypic"
+            className="w-full h-235 border-tl-12 border-tr-12"
+          />
+          <div className="bg-white border-tl-12 border-tr-12 p-6">
+            <h3 className="mt-5 mb-10 font-raleway bg-white capitalize">
+              {props.fname} <span className="ml-16">{props.price}</span>
+            </h3>
 
-         
+            <span className="uppercase font-raleway text-xs tracking-widest font-medium text-gray-600 bg-white">
+              {props.title}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
-export default ExploreFood 
+export default ExploreFood;
--- src/components/ExploreFoodData.jsx
+++ src/components/ExploreFoodData.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import ExploreFood from './ExploreFood'
-import { foodData } from '../data/foodData'
+import React from "react";
+import ExploreFood from "./ExploreFood";
+import { foodData } from "../data/foodData";
 export const ExploreFoodData = () => {
   return (
-    <>
-  <h1 className="heading_style py-8 text-center font-raleway tracking-wide font-semibold text-2xl mb-8 bg-white text-gray-500">Explore our Foods</h1>
-  {foodData.map((item, index) => (
-    <ExploreFood key={index} imgsrc={item.imgsrc} fname={item.fname} title={item.title} price={item.price} />
-  ))}
-
-
-
-  
-</>
-
-  )
-}
+    <div>
+      <h1 className="py-8 text-center font-raleway tracking-wide font-semibold text-2xl mb-8 bg-white text-gray-500">
+        Explore our Foods
+      </h1>
+      <div className="flex w-full flex-wrap justify-center">
+        {foodData.map((item, index) => (
+          <ExploreFood
+            key={index}
+            imgsrc={item.imgsrc}
+            fname={item.fname}
+            title={item.title}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
