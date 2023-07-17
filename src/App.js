2d50f8860ee484624f3b9038b59d9fdac9023e91
nidhi <aakritinidhi123@gmail.com>
2023-07-17 18:12:52 +0530

    add service

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import TopResturantSection from "./components/TopResturantSection";
 import { ExploreFoodData } from "./components/ExploreFoodData";
 import  AddToCardSection  from "./components/AddToCardSection";
 import OurServices from "./components/OurServices";
+import { BookingPage } from "./components/BookingPage";
 function App() {
   return (
     <div>
       <HeroSection />
       <TopResturantSection />
+      <BookingPage/>
       <OurServices/>
       <ExploreFoodData/>
       <AddToCardSection/>
--- src/components/OurServices.jsx
+++ src/components/OurServices.jsx
@@ -4,6 +4,9 @@ function OurServices() {
     return (
         <>
             <div className="w-full h-screen">
+
+
+
                 <div className="left w-1/2 ml-10 mt-20 flex items-center">
                     <div className="w-40 h-5 bg-teal-600 transform rotate-90"></div>
                     <h1 className="text-6xl font-bold text-purple-900 px-10 leading-tight ml-4">
@@ -23,15 +26,55 @@ function OurServices() {
                             ullamco laboris nisi ut aliquip ex ea commodo consequat.
                         </p>
                     </div>
-                    <div className="" style={{ backgroundImage: 'url(./dots-bg2.svg)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
-                        <div >
-                        <img src="/advanced-table-booking.svg" className="ml-2" alt="table" />
+                </div>
+                
+             
+                
+                <div className="h-screen lg:h-full bg-cover bg-center" style={{ backgroundImage: "url('./dots-bg2.svg')", backgroundSize: "120%" }}>
+                    <div className="flex">
+                        <img src="/advanced-table-booking.svg" className="ml-2 px-40 py-10" alt="table" />
+                        <img src="/big-left-arrow.svg" className="" />
+                        <div className="mt-20 ml-4">
+                            <p className="text-2xl font-bold text-purple-900">
+                                advanced table
+                            </p>
+                            <p className="text-2xl font-bold text-purple-900">
+                                booking ------
+                            </p>
+                            <p className="text-sm text-gray-500">
+                                Lorem Ipsum dolor sit
+                            </p>
+                            <p className="text-sm text-gray-500">
+                                amet,consectetur
+                            </p>
+                            <p className="text-sm text-gray-500">
+                                adipiscing elit, sed do
+                            </p>
                         </div>
-                        
                     </div>
 
+                    <div className="flex">
+                        <div className="ml-2 px-40 py-10">
+                            <p className="text-2xl font-bold text-purple-900">
+                                Food for Free
+                            </p>
+                            <p className="text-2xl font-bold text-purple-900">
+                                --------24/7
+                            </p>
+                            <p className="text-sm text-gray-500">
+                                Lorem Ipsum dolor sit
+                            </p>
+                            <p className="text-sm text-gray-500">
+                                amet,consectetur
+                            </p>
+                            <p className="text-sm text-gray-500">
+                                adipiscing elit, sed do
+                            </p>
+                        </div>
+                        <img src="/big-left-arrow.svg" className="transform rotate-180" />
+                        <img src="/food-for-free.svg" className="mt-5 ml-4" alt="table" />
+                    </div>
                 </div>
-
             </div>
         </>
     )
